098f8396d5f5511ccd942ea0cc156e8f885fa5f1
Stefan <stefan.guesten@gmx.de>
2013-04-27 00:45:31 +0200

    HTML Parsen - aktualisiert (neuer Ansatz)
    Bessere Implementation durch Zusammenarbeit mit Wolf, Niklas und Agata.
    Dokumentation besser, aber noch unvollständig. Ab jetzt von jedem
    nachvollziehbar.
    
    Weitere 4 Stunden programmiert.

--- Release 1/1 2/openhtml.py
+++ Release 1/1 2/openhtml.py
@@ -1,52 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""Vorgehen
-	* hmtlListe importieren
-	* Oeffenen einer html 
-	* Oeffenen aller html @TODO noch nicht gemacht. For-schleife
-	* Einfacher Parser
-	* speichern als textdatei mitbeliebiger Endung
-
-"""
-
 import os
 import sys
 import urllib
-import re #Regulaere Ausdruecke
+import re
 
 from HTMLParser import HTMLParser
 
 from drwHelper import openPickle
 from drwHelper import save
 from drwHelper import convertHTMLChars
-from drwHelper import testParser
-
-
 
 def loadHTMLList():
 	"""
-	@return a list [] of all html urls
+	@return a list[] of all html urls
 	"""
 	htmlList = openPickle(os.getcwd(), "listOfAllHTMLSites.pickle")
-#	for url in htmlList:
-#		print url
 	
 	return htmlList
 
 
 def getTextOfHTML(url):
-	"""opens an url and returns as a string
-	
+	"""
+	opens an url and returns its content as a string
 	@return The Content of the URL as a string
-	
 	"""
 	oneHTML = urllib.urlopen(url)
 	hTMLText = oneHTML.read()
-#	print oneHTML.info()
 	
 	return hTMLText
-
 
 
 # create a subclass and override the handler methods
@@ -54,40 +37,93 @@ class DrwHTMLParser(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
 		self.myText = u''
+		self.currentTag = "" 
+		self.currentAttrs = ""
 	def handle_starttag(self, tag, attrs):
 #        print "Encountered a start tag:", tag
 #        print "With attributes:", attrs
-		pass
+		self.currentTag = tag
+		self.currentAttrs = attrs
+#		print attrs
 	def handle_endtag(self, tag):
-#        print "Encountered an end tag :", tag
-		pass
+		self.currentTag = "x"		
 	def handle_data(self, data):
 #        print "Encountered some data  :", data
 		if data.strip() != "":
-#			print (data.strip())
-			self.myText += data.strip()+"\n"
-#		else:
-#			print "\nZZZ",data.strip(), "ZZZ"
+			if self.currentTag == "b":
+				self.myText += "<b>"
+				self.myText += data
+				self.myText += "</b>"	
+			elif self.currentTag == "i":
+				self.myText += "<i>"
+				self.myText += data
+				self.myText += "</i>"
+			elif self.currentTag == "a":
+				link = ""
+				for attr in self.currentAttrs:
+					if attr[0] == "href":
+						link = attr[1]
+						foundInternLink = re.search("\d{5}\.html", link)
+						if foundInternLink:
+							personNumber = re.search("\d{5}", link).group()				
+							self.myText += "#internNameLink#" + personNumber + "#endNameNr#"
+							self.myText += data + "#/internNameLink#"		
+						else:
+							self.myText += " " + link + " "
+							self.myText = self.myText.replace("  "," ")
+			elif self.currentTag == "x":
+				self.myText += data
+			else:
+				self.myText += "\n" + data
+		self.myText = self.myText.replace("\n\n","\n")
 
 
 
-def parser(feed):								#Parser instanziieren und HTM übergeben
+def parser(feed):
+	"""
+	@return parsed String
+	"""
 	parser = DrwHTMLParser()
 	parser.feed(feed)
+	
 	return parser.myText
 
 
-def main(argv):
-	htmlList = loadHTMLList()					#Laedt die Liste
+def deleteCSS(text):
+	"""
+	@return cleaned text as a string
+	"""
+	zeilen = text.splitlines(True)
+	startZeile = 1
+	endZeile = -1
 	
-#	for i in range(3):							#Testlauf mit den ersten urls
-	for i in range(len(htmlList)):				#durchlaufe alle URLs
-#		htmlText = getTextOfHTML(htmlList[i])	#oeffnet die i-te url aus der Liste
-#		htmlText = convertHTMLChars(htmlText)	#nimmt die htmlEntities raus
-#		text = parser(htmlText)					#Versuchs Parser
-#		save(text, os.getcwd()+"/Temp", "person_"+str(i),".txt")
-		testParser(i)	
+	for i in range(len(zeilen)):
+		if ("personendatenbank.html" in zeilen[i]):
+			endZeile = i+1
+			break
+	
+	for i in range(startZeile, endZeile):
+		zeilen[i] = ""
+		
+	for i in range(len(zeilen)):
+		if zeilen[i].strip() == "":
+			zeilen[i] = ""
+	
+	text = "".join(zeilen[:])
+	
+	return text
 
+
+def main(argv):
+	htmlList = loadHTMLList()						#loads the url list	
+#	for i in range(2):							#loop for testing purposes
+	for i in range(len(htmlList)):						#loop through the urls
+		htmlText = getTextOfHTML(htmlList[i])				#get text of i-th htm file
+		htmlText = convertHTMLChars(htmlText)				#convert speacial htm characters to unicode
+		text = htmlList[i][26:31] + "\n"				#add the file number to the new text 
+		text += parser(htmlText)					#delete certain htm tags
+		text = deleteCSS(text)						#delete CSS code and some whitespaces
+		save(text, os.getcwd()+"/Temp", "person_"+str(i),".txt")	#save the new text in a subfolder
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
